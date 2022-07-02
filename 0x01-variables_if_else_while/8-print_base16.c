5f9bfca244023f248edd2393a074a1cd79902e3d
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 05:31:11 -0700

    Hex it

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+
+		putchar(base_16);
+
+	for (base_16 = 97; base_16 <= 122; base_16++)
+
+		putchar(base_16);
+
+	putchar('\n');
+
+	return (0);
+}
