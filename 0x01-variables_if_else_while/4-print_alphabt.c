c3835a734b310edbd4bedc5b81ecbe3e160b1024
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 03:23:52 -0700

    Testing with or

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: print lowercase alphabets except q and e
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if (alphabet == 'q' || alphabet == 'e')
+			continue;
+		else
+			putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
+
