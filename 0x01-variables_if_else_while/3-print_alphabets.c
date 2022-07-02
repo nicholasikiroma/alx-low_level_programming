d3ddbde2b811c7eea3d3883e5d7fbecb9cc7e275
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 02:47:25 -0700

    For loops

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,16 @@
 
 int main(void)
 {
-	char x;
+	char lower_case;
+	char upper_case;
 
-	for (x = 'a'; x <= 'z'; x++)
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-		putchar(x);
-		putchar(toupper(x));
+		putchar(lower_case);
+	}
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
 	}
 	putchar('\n');
 	return (0);
