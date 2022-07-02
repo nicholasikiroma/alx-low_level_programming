a0e3b788c9f86b3b0296ea78c73bc7fd4c7e546a
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 04:56:10 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n <= 'a'; n++)
+
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
