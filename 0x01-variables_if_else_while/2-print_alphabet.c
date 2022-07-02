449002ef1614e1bdf7fa9f7bd30ba51f5f3aca28
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 01:44:20 -0700

    conditional statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: loop to print alphabets in lowercase using  putchar()
+ * Return: 0 Always success
+ */
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0)
+}
