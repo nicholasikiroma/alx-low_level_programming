3b6a485e7d78af8c23088278b809c391a64cbf9c
Nicholas <nicholasikiroma@gmail.com>
2022-07-02 04:11:06 -0700

    Numbers fixed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+
+		int putchar(numbers);
+	putchar('\n');
+
+	return (0);
+}
