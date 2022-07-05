d9fde61e8f5b5611d4df9540773e0ee4acaf4337
Nicholas <nicholasikiroma@gmail.com>
2022-07-05 05:42:43 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,12 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[] = "_putchar";
+	int msg;
+
+	for (msg = 0; msg <= 8; msg++)
+		_putchar(c[msg]);
+
 	_putchar('\n');
 
 	return (0);
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h" 
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success 
+ */
+
+int main(void)
+{
+
+}
