dfc57ffbd6692812f13fe94adff99226bc8e3c34
Nicholas <nicholasikiroma@gmail.com>
2022-07-13 17:01:59 -0700

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,10 +10,33 @@
 
 int main(void)
 {
-	int pass;
+	int a, j, b, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	srand(time(0));
-	pass = rand();
-	printf("%i\n", pass);
+	srand(time(NULL));
+	while (s != 2772)
+	{
+		a = b = s = 0;
+		while ((2772 - 122) > s)
+		{	j = rand() % 62;
+			p[a] = c[j];
+			s += c[j];
+			a++;
+		}
+		while (c[b])
+		{
+			if (c[b] == (2772 - s))
+			{
+				p[a] = c[b];
+				s += c[b];
+				a++;
+				break;
+			}
+			b++;
+		}
+	}
+	p[a] = '\0';
+	printf("%s\n", p);
 	return (0);
 }
