246e1a83b7bcb62d2be22ed9d1b7552190c35ffa
Nicholas <nicholasikiroma@gmail.com>
2022-08-01 10:12:47 -0700

    First commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(X) ((X) < 0 ? (-(X)) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This directory contains implementation of Macros and Preprocessor directives in C
