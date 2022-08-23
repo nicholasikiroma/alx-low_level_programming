f8f31449988ed3b46a3c27f01220371e66cd270c
Nicholas <nicholasikiroma@gmail.com>
2022-08-22 20:43:53 -0700

    bug fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fo = open(filename, O_CREAT | O_RDWR | O_Trunc, 0600);
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fo, text_content, len);
 
 	if (fo == -1 || fw == -1)
